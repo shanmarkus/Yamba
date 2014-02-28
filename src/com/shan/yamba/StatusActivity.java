6381ebd9a08014c3371bf19a5fa139f6a0a4ae1f
shanmarkus <shanmarkus@gmail.com>
2014-02-28 18:20:19 +0700

    First Commit
    Finishing To Chapter 6

---
+++ src/com/shan/yamba/StatusActivity.java
@@ -0,0 +1,98 @@
+package com.shan.yamba;
+
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher { //
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+	TextView textCount; //
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+
+		editText = (EditText) findViewById(R.id.editText);
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+
+		updateButton.setOnClickListener(this);
+		textCount = (TextView) findViewById(R.id.textCount);
+		textCount.setText(Integer.toString(140));
+		textCount.setTextColor(Color.GREEN);
+		editText.addTextChangedListener(this);
+
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://yamba.shan.com/api");
+	}
+
+	class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... statuses) {
+			// TODO Auto-generated method stub
+			try {
+				winterwell.jtwitter.Status status = twitter
+						.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG);
+		}
+
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
+		Log.d(TAG, "onClicked");
+	}
+
+	// TextWatcher methods
+	public void afterTextChanged(Editable statusText) { //
+		int count = 140 - statusText.length(); //
+		textCount.setText(Integer.toString(count));
+		textCount.setTextColor(Color.GREEN); //
+		if (count < 10)
+			textCount.setTextColor(Color.YELLOW);
+		if (count < 0)
+			textCount.setTextColor(Color.RED);
+	}
+
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+	}
+	public void onTextChanged(CharSequence s, int start, int before, int count) { // 
+		}
+	}
+
