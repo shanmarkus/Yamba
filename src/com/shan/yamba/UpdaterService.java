50a88fff7972ffb1c58ca0e671026f19c1495b3b
shanmarkus <shanmarkus@gmail.com>
2014-03-01 16:20:47 +0700

    page 113

--- src/com/shan/yamba/UpdaterService.java
+++ src/com/shan/yamba/UpdaterService.java
@@ -7,6 +7,9 @@ import android.util.*;
 
 public class UpdaterService extends Service {
 	static final String TAG = "UpdaterService";
+	static final int DELAY = 60000;
+	private boolean runFlag = false;
+	private Updater updater;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -16,18 +19,42 @@ public class UpdaterService extends Service {
 
 	public void onCreate() {
 		super.onCreate();
+		this.updater = new Updater();
 		Log.d(TAG, "onCreated");
 	}
 
 	public int onStartCommand(Intent intent, int flags, int startId) { //
+		this.runFlag = true;
 		super.onStartCommand(intent, flags, startId);
 		Log.d(TAG, "onStarted");
 		return START_STICKY;
 	}
-	
-	public void onDestroy(){
+
+	public void onDestroy() {
+		this.updater.interrupt();
+		this.runFlag = false;
 		super.onDestroy();
-		Log.d(TAG, "onDestroy");
+		Log.d(TAG, "onDestroyed");
 	}
 
+	private class Updater extends Thread {
+		public Updater() {
+			super("UpdaterService-Updater");
+		}
+
+		@Override
+		public void run() { //
+			UpdaterService updaterService = UpdaterService.this;
+			while (updaterService.runFlag) {
+				Log.d(TAG, "Updater running");
+				try {
+					// Some work goes here...
+					Log.d(TAG, "Updater ran");
+					Thread.sleep(DELAY);
+				} catch (InterruptedException e) { //
+					updaterService.runFlag = false;
+				}
+			}
+		}
+	}
 }
