01b6dae520de521699a5e937b1b854701153dd6a
shanmarkus <shanmarkus@gmail.com>
2014-03-01 15:00:35 +0700

    Page 100
    Ending chapter 8

--- src/com/shan/yamba/StatusActivity.java
+++ src/com/shan/yamba/StatusActivity.java
@@ -4,9 +4,12 @@ import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -21,12 +24,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener,
-		TextWatcher { //
+		TextWatcher, OnSharedPreferenceChangeListener {
 	private static final String TAG = "StatusActivity";
 	EditText editText;
 	Button updateButton;
 	Twitter twitter;
-	TextView textCount; //
+	TextView textCount;
+	SharedPreferences prefs;//
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,7 +43,7 @@ public class StatusActivity extends Activity implements OnClickListener,
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) { //
 		case R.id.itemPrefs:
-			startActivity(new Intent(this, PrefsActivity.class)); // 
+			startActivity(new Intent(this, PrefsActivity.class)); //
 			break;
 		}
 		return true; //
@@ -58,9 +62,24 @@ public class StatusActivity extends Activity implements OnClickListener,
 		textCount.setText(Integer.toString(140));
 		textCount.setTextColor(Color.GREEN);
 		editText.addTextChangedListener(this);
+		prefs = PreferenceManager.getDefaultSharedPreferences(this); //
+		prefs.registerOnSharedPreferenceChangeListener(this);
 
 		twitter = new Twitter("student", "password");
 		twitter.setAPIRootUrl("http://yamba.shan.com/api");
+	}
+
+	private Twitter getTwitter() {
+		if (twitter == null) { //
+			String username, password, apiRoot;
+			username = prefs.getString("username", ""); //
+			password = prefs.getString("password", "");
+			apiRoot = prefs.getString("apiRoot", "http://yamba.shan.com/api");
+			// Connect to twitter.com
+			twitter = new Twitter(username, password); //
+			twitter.setAPIRootUrl(apiRoot); //
+		}
+		return twitter;
 	}
 
 	class PostToTwitter extends AsyncTask<String, Integer, String> {
@@ -94,10 +113,11 @@ public class StatusActivity extends Activity implements OnClickListener,
 	}
 
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		String status = editText.getText().toString();
-		new PostToTwitter().execute(status);
-		Log.d(TAG, "onClicked");
+		try {
+			getTwitter().setStatus(editText.getText().toString());
+		} catch (TwitterException e) {
+			Log.d(TAG, "Twitter setStatus failed: " + e);
+		}
 	}
 
 	// TextWatcher methods
@@ -116,5 +136,9 @@ public class StatusActivity extends Activity implements OnClickListener,
 	}
 
 	public void onTextChanged(CharSequence s, int start, int before, int count) { //
+	}
+
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) { 
+		twitter = null;
 	}
 }
