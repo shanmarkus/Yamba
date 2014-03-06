0f265b22b5159d35d4e2dc0bcf2d54d21a3494f3
shanmarkus <shanmarkus@gmail.com>
2014-03-06 15:04:34 +0700

    page 127

--- src/com/shan/yamba/UpdaterService.java
+++ src/com/shan/yamba/UpdaterService.java
@@ -3,7 +3,9 @@ package com.shan.yamba;
 import java.util.List;
 
 import android.app.Service;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.IBinder;
 import android.util.*;
 import winterwell.jtwitter.Twitter;
@@ -17,6 +19,9 @@ public class UpdaterService extends Service {
 	private Updater updater;
 	private YambaApplication yamba;
 
+	DbHelper dbHelper;
+	SQLiteDatabase db;
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
@@ -27,6 +32,7 @@ public class UpdaterService extends Service {
 		super.onCreate();
 		this.updater = new Updater();
 		this.yamba = (YambaApplication) getApplication();
+		dbHelper = new DbHelper(this);
 		Log.d(TAG, "onCreated");
 	}
 
@@ -64,15 +70,30 @@ public class UpdaterService extends Service {
 				try {
 					// Some work goes here...
 					try {
-						timeline = yamba.getTwitter().getFriendsTimeline(); 
+						timeline = yamba.getTwitter().getFriendsTimeline();
 					} catch (TwitterException e) {
 						Log.e(TAG, "Failed to connect to twitter service", e);
 					}
-					//looping to get the twittaahhhh
-					for (winterwell.jtwitter.Status status : timeline) { //
+
+					db = dbHelper.getWritableDatabase();
+					ContentValues values = new ContentValues();
+					// looping to get the twittaahhhh
+					for (winterwell.jtwitter.Status status : timeline) { 
+						//inputing values
+						values.clear();
+						values.put(DbHelper.C_ID, (status.id).toString());
+						values.put(DbHelper.C_CREATED_AT,
+								status.createdAt.getTime());
+						values.put(DbHelper.C_SOURCE, status.source);
+						values.put(DbHelper.C_TEXT, status.text);
+						values.put(DbHelper.C_USER, status.user.name);
+						
+						db.insertOrThrow(dbHelper.TABLE, null, values);
+						//log
 						Log.d(TAG, String.format("%s: %s", status.user.name,
 								status.text)); //
 					}
+					db.close();
 					Log.d(TAG, "Updater ran");
 					Thread.sleep(DELAY);
 				} catch (InterruptedException e) { //
